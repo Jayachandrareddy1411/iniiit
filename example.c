0fb9402b5938d2a536dc52f5ca238c59c0ffbeaa
Jayachandrareddy1411 <jaychilakamarry@gmail.com>
2025-01-02 22:45:18 +0530

    1

---
+++ example.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<fcntl.h>
+
+
+int main() {
+	
+	int fd;
+	char buffer[50];
+	char msg[50] = "Hello this is jayachandra";
+	fd=open("content3.txt",O_RDWR | O_CREAT);
+	
+	printf("fd: %d", fd);
+	
+	if(fd!=-1) {
+		printf("\n content2.txt opened with RW access\n");
+		write(fd,msg,sizeof(msg));
+		lseek(fd,0,SEEK_SET);
+		read(fd,buffer,sizeof(msg));
+		printf("\n %s was written to my files\n", buffer);
+		close(fd);
+	}
+	
+	return 0;
+}
